b990e023c10d8ab62d5d4bbacc772ef0a5fd2627
Cristian Mello <cristianc.mello@gmail.com>
2020-06-18 13:22:44 -0300

    Update docker-cleanup.sh

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,11 +1,15 @@
-# ~/.bash_aliases
+#!/bin/bash
 
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker volume rm $(docker volume ls -qf dangling=true)
-docker volume ls -qf dangling=true | xargs -r docker volume rm
-docker rmi -f $(docker images -q -f dangling=true)
-docker rmi -f $(docker images --filter "dangling=true" -q --no-trunc)
-docker rmi -f $(docker images | grep "none" | awk '/ / { print $3 }')
-docker rmi -f $(docker images -q)
-docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+DOCKER_CONTAINERS=$(docker ps -a -q)
+
+if [[ -z $DOCKER_CONTAINERS ]]; then
+  echo "Containers: nothing to stop"
+else
+  docker stop $DOCKER_CONTAINERS
+fi
+
+docker image prune -a --force
+docker container prune --force
+docker network prune --force
+docker volume prune --force
+docker system prune --volumes --force
