14ceadb7e25d222d97c228501e3f44bd18842927
Cristian Mello <cristianc.mello@gmail.com>
2020-09-13 16:31:02 -0300

    Update filewatcher.sh

--- filewatcher.sh
+++ filewatcher.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-while true; do 
-  inotifywait -qrm --event modify --format '%w' *.st | 
-  gst hello.st;
-  clear; 
+PID="-1"
+FILE_EXT="$1"
+SCRIPT_WILL_BE_EXECUTED="$2"
+
+mainAction() {
+  [[ $PID != "-1" ]] && kill -9 $PID 2>/dev/null;
+  $SCRIPT_WILL_BE_EXECUTED &
+  PID=$!;
+}
+
+mainAction
+
+while inotifywait -e modify $FILE_EXT; do
+  mainAction
 done
